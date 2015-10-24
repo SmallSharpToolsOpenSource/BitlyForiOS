ab034b65d014dd51141ddd7474b798ea9e0b4448
brennanMKE <brennan@smallsharptools.com>
2015-10-23 17:41:03 -0700

    prepared to work with Carthage

---
+++ BitlyForiOS/BitlyForiOS/BitlyForiOS.h
@@ -0,0 +1,19 @@
+//
+//  BitlyForiOS.h
+//  BitlyForiOS
+//
+//  Created by Brennan Stehling on 10/23/15.
+//  Copyright © 2015 SmallSharpTools LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitlyForiOS.
+FOUNDATION_EXPORT double BitlyForiOSVersionNumber;
+
+//! Project version string for BitlyForiOS.
+FOUNDATION_EXPORT const unsigned char BitlyForiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitlyForiOS/PublicHeader.h>
+
+#import <BitlyForiOS/SSTURLShortener.h>
